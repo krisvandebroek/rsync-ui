2cc051c2915d89ba7a6c8d3a1791ae0e395d41cf
Kris Vandebroek <kris.vandebroek@aca-it.be>
2014-12-30 17:11:33 +0100

    File sizes vary between systems. Adapted test to be more robust

--- public/test/fileBrowserSpec.js
+++ public/test/fileBrowserSpec.js
@@ -15,7 +15,7 @@ describe('FileBrowser', function () {
                 expect(node).to.exist();
                 expect(node.path).to.equal(dummyDirPath);
                 expect(node.isDirectory).to.equal(true);
-                expect(node.bytes).to.equal(136);
+                expect(node.bytes).to.be.greaterThan(0);
                 expect(node.collapsed).to.equal(true);
                 done();
             });
@@ -29,7 +29,7 @@ describe('FileBrowser', function () {
                 expect(node).to.exist();
                 expect(node.path).to.equal(filePath);
                 expect(node.isDirectory).to.equal(false);
-                expect(node.bytes).to.equal(10);
+                expect(node.bytes).to.be.greaterThan(0);
                 expect(node.collapsed).to.equal(true);
                 done();
             });
@@ -49,11 +49,11 @@ describe('FileBrowser', function () {
                     expect(node.children).to.have.length(2);
                     expect(node.children[0].path).to.equal(path.join(dummyDirPath, 'dummyFile1.txt'));
                     expect(node.children[0].isDirectory).to.equal(false);
-                    expect(node.children[0].bytes).to.equal(0);
+                    expect(node.children[0].bytes).to.be.equal(0);
                     expect(node.children[0].collapsed).to.equal(true);
                     expect(node.children[1].path).to.equal(path.join(dummyDirPath, 'dummySubdir1'));
                     expect(node.children[1].isDirectory).to.equal(true);
-                    expect(node.children[1].bytes).to.equal(102);
+                    expect(node.children[1].bytes).to.be.greaterThan(0);
                     expect(node.children[1].collapsed).to.equal(true);
                     done();
                 })
@@ -68,7 +68,7 @@ describe('FileBrowser', function () {
                 expect(node).to.exist();
                 expect(node.path).to.equal(filePath);
                 expect(node.isDirectory).to.equal(false);
-                expect(node.bytes).to.equal(10);
+                expect(node.bytes).to.be.greaterThan(0);
                 expect(node.collapsed).to.equal(true);
                 done();
             });
